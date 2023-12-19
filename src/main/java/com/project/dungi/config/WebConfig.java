d403dcb195a2669ccd041d8f7f95afb53ccd56ee
diydriller <dhrhd080@naver.com>
2023-12-19 19:03:12 +0900

    ADD config 파일

---
+++ src/main/java/com/project/dungi/config/WebConfig.java
@@ -0,0 +1,46 @@
+package com.project.dungi.config;
+
+import com.project.dungi.web.Interceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final Interceptor interceptor;
+
+    private final String[] INTERCEPTOR_EXCLUDE_LIST={
+            "/check/email",
+            "/phone",
+            "/check/phone",
+            "/user",
+            "/login",
+            "/kakao/user",
+            "/kakao/callback",
+            "/kakao/login"
+    };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(INTERCEPTOR_EXCLUDE_LIST);
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations( "file:src/main/resources/static/");
+    }
+}
