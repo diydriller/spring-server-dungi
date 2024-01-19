4fbe4215855cd6cc759e7c04c1cb11c2a16b7eb8
diydriller <dhrhd080@naver.com>
2024-01-19 19:11:03 +0900

    UPDATE 멀티 모듈 적용하면서 sms 모듈 생성

--- infrastructure-sms/src/main/java/com/dungi/sms/infrastructure/twilio/TwilioSmsSender.java
+++ infrastructure-sms/src/main/java/com/dungi/sms/infrastructure/twilio/TwilioSmsSender.java
@@ -1,2 +1,29 @@
-package com.dungi.sms.infrastructure.twilio;public class TwilioSmsSender {
+package com.dungi.sms.infrastructure.twilio;
+
+import com.dungi.core.domain.sms.SmsSender;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TwilioSmsSender implements SmsSender {
+
+    @Value("${twilio.accountId}")
+    private String twilioAccountSid;
+    @Value("${twilio.authToken}")
+    private String twilioAuthToken;
+    @Value("${twilio.adminPhoneNumber}")
+    private String adminPhoneNumber;
+
+    @Override
+    public void sendSms(String phoneNumber, String randomNumber) {
+        Twilio.init(twilioAccountSid, twilioAuthToken);
+        Message.creator(
+                        new PhoneNumber(phoneNumber),
+                        new PhoneNumber(adminPhoneNumber),
+                        "enter the number : " + randomNumber)
+                .create();
+    }
 }
