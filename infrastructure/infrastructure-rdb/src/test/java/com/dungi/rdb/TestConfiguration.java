1b83d6d45e4d7722620603ab74539dd46d1275ba
diydriller <dhrhd080@naver.com>
2024-11-17 23:25:41 +0900

    fix: repository 패키지 경로 수정

--- infrastructure/infrastructure-rdb/src/test/java/com/dungi/rdb/TestConfiguration.java
+++ infrastructure/infrastructure-rdb/src/test/java/com/dungi/rdb/TestConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.dungi.rdb.repository")
+@EnableJpaRepositories(basePackages = "com.dungi.rdb.jpa.repository")
 @EntityScan(basePackages = "com.dungi.core.domain")
 @SpringBootApplication
 public class TestConfiguration {
