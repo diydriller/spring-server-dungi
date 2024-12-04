f8362f7aa459ca9f408c84e7f482e26c82fe0d61
diydriller <dhrhd080@naver.com>
2024-12-04 19:20:09 +0900

    fix: spring batch 성능 개선 - JpaPagingItemWriter에서 JpaCursorItemReader로 변경 - JpaItemWriter에서 JdbcBatchItemWriter로 변경

--- batch-server/src/main/java/com/dungi/batchserver/batch/job/JobConfig.java
+++ batch-server/src/main/java/com/dungi/batchserver/batch/job/JobConfig.java
@@ -1,6 +1,5 @@
 package com.dungi.batchserver.batch.job;
 
-import com.dungi.batchserver.batch.writer.JpaItemListWriter;
 import com.dungi.core.domain.common.value.DeleteStatus;
 import com.dungi.core.domain.room.model.Room;
 import com.dungi.core.domain.summary.model.WeeklyTopUser;
@@ -11,13 +10,16 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.database.JpaItemWriter;
-import org.springframework.batch.item.database.JpaPagingItemReader;
-import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 import java.time.LocalDate;
 import java.time.temporal.WeekFields;
 import java.util.HashMap;
@@ -32,33 +34,35 @@ public class JobConfig {
     private final EntityManagerFactory entityManagerFactory;
     private final WeeklyStatisticStore weeklyStatisticStore;
     private final StepBuilderFactory steps;
-    private final int chunkSize = 10;
+    private final DataSource dataSource;
+
+    private final static int CHUNK_SIZE = 10;
 
     @Bean
-    public Step decideBestMemberStep() {
-        return steps.get("decideBestMemberStep")
-                .<Room, List<WeeklyTopUser>>chunk(chunkSize)
-                .reader(decideBestMemberPagingReader())
-                .processor(decideBestMemberProcessor())
-                .writer(decideBestMemberListWriter())
+    public Step decideWeeklyTopUserStep() {
+        return steps.get("decideWeeklyTopUserStep")
+                .<Room, List<WeeklyTopUser>>chunk(CHUNK_SIZE)
+                .reader(decideWeeklyTopUserReader())
+                .processor(decideWeeklyTopUserProcessor())
+                .writer(decideWeeklyTopUserListWriter(dataSource))
                 .build();
     }
 
     @Bean
-    public JpaPagingItemReader<Room> decideBestMemberPagingReader() {
+    public JpaCursorItemReader<Room> decideWeeklyTopUserReader() {
         Map<String, Object> parameterValues = new HashMap<>();
         parameterValues.put("deleteStatus", DeleteStatus.NOT_DELETED);
-        return new JpaPagingItemReaderBuilder<Room>()
-                .queryString("SELECT r FROM Room r WHERE r.deleteStatus = :deleteStatus")
-                .pageSize(chunkSize)
+
+        return new JpaCursorItemReaderBuilder<Room>()
+                .queryString("SELECT r FROM Room r WHERE r.deleteStatus = :deleteStatus ORDER BY r.id ASC")
                 .entityManagerFactory(entityManagerFactory)
                 .parameterValues(parameterValues)
-                .name("decideBestMemberPagingReader")
+                .name("decideWeeklyTopUserCursorReader")
                 .build();
     }
 
     @Bean
-    public ItemProcessor<Room, List<WeeklyTopUser>> decideBestMemberProcessor() {
+    public ItemProcessor<Room, List<WeeklyTopUser>> decideWeeklyTopUserProcessor() {
         return room -> {
             var lastWeekDate = LocalDate.now().minusWeeks(1);
             var weekFields = WeekFields.ISO;
@@ -66,31 +70,38 @@ public class JobConfig {
             var weekOfYear = lastWeekDate.get(weekFields.weekOfYear());
 
             return weeklyStatisticStore.decideAndGetWeeklyTopUserInRoom(room.getId(), year, weekOfYear).stream()
-                    .map(weeklyTodoCount -> {
-                        var user = WeeklyTopUser.builder()
-                                .userId(weeklyTodoCount.getUserId())
-                                .roomId(weeklyTodoCount.getRoomId())
-                                .weekOfYear(weeklyTodoCount.getWeekOfYear())
-                                .year(weeklyTodoCount.getYear())
-                                .build();
-                        System.out.println(user);
-                        return user;
-                    })
-                    .collect(Collectors.toList());
+                    .map(weeklyTodoCount ->
+                            WeeklyTopUser.builder()
+                                    .userId(weeklyTodoCount.getUserId())
+                                    .roomId(weeklyTodoCount.getRoomId())
+                                    .weekOfYear(weeklyTodoCount.getWeekOfYear())
+                                    .year(weeklyTodoCount.getYear())
+                                    .build()
+                    ).collect(Collectors.toList());
         };
     }
 
     @Bean
-    public JpaItemListWriter<WeeklyTopUser> decideBestMemberListWriter() {
-        JpaItemWriter<WeeklyTopUser> writer = new JpaItemWriter<>();
-        writer.setEntityManagerFactory(entityManagerFactory);
-        return new JpaItemListWriter<>(writer, entityManagerFactory);
+    public ItemWriter<List<WeeklyTopUser>> decideWeeklyTopUserListWriter(DataSource dataSource) {
+        return items -> {
+            List<WeeklyTopUser> flatList = items.stream()
+                    .flatMap(List::stream)
+                    .collect(Collectors.toList());
+
+            JdbcBatchItemWriter<WeeklyTopUser> writer = new JdbcBatchItemWriter<>();
+            writer.setDataSource(dataSource);
+            writer.setSql("INSERT INTO weekly_top_user (created_time, modified_time, room_id, user_id, week_of_year, year) VALUES (now(), now(), :roomId, :userId, :weekOfYear, :year)");
+            writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+            writer.afterPropertiesSet();
+
+            writer.write(flatList);
+        };
     }
 
     @Bean(name = "decideBestMateJob")
-    public Job decideBestMemberJob() {
-        return job.get("decideBestMemberJob")
-                .start(decideBestMemberStep())
+    public Job decideWeeklyTopUserJob() {
+        return job.get("decideWeeklyTopUserJob")
+                .start(decideWeeklyTopUserStep())
                 .build();
     }
 }
