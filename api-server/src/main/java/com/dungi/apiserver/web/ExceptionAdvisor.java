f502ce884865cb23bd8cc32042f7d6f823a719f8
diydriller <dhrhd080@naver.com>
2024-12-13 13:39:53 +0900

    fix: 필요없는 reponse body annotation 제거

--- api-server/src/main/java/com/dungi/apiserver/web/ExceptionAdvisor.java
+++ api-server/src/main/java/com/dungi/apiserver/web/ExceptionAdvisor.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -28,25 +27,24 @@ public class ExceptionAdvisor {
     // 입력값 검증 에러 핸들러
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
-    @ResponseBody
-    public BaseResponse<?> handleValidationException(BindException e){
+    public BaseResponse<?> handleValidationException(BindException e) {
         String eventId = MDC.get(REQUEST_KEY);
         log.info("eventId = {} ", eventId, e);
         BindingResult result = e.getBindingResult();
         List<String> errorList = new ArrayList<>();
         result.getFieldErrors().forEach((fieldError) ->
-            errorList.add(
-                    fieldError.getField() +
-                            " : " +
-                            fieldError.getDefaultMessage() +
-                            " : rejected value is " +
-                            fieldError.getRejectedValue()
-            )
+                errorList.add(
+                        fieldError.getField() +
+                                " : " +
+                                fieldError.getDefaultMessage() +
+                                " : rejected value is " +
+                                fieldError.getRejectedValue()
+                )
         );
 
         return BaseResponse.builder()
                 .isSuccess(false)
-                .message(String.join(" / ",errorList))
+                .message(String.join(" / ", errorList))
                 .code(400)
                 .build();
     }
@@ -54,8 +52,7 @@ public class ExceptionAdvisor {
     // 헤더에서 토큰이 없거나 토큰 검증 실패시 에러 핸들러
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(AuthenticationException.class)
-    @ResponseBody
-    public BaseResponse<?> handleAuthenticationException(AuthenticationException e){
+    public BaseResponse<?> handleAuthenticationException(AuthenticationException e) {
         String eventId = MDC.get(REQUEST_KEY);
         log.info("eventId = {} ", eventId, e);
         return new BaseResponse<>(e.getStatus());
@@ -64,8 +61,7 @@ public class ExceptionAdvisor {
     // 리소스가 없을 경우 에러 핸들러
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    @ResponseBody
-    public BaseResponse<?> handleNotFoundException(NotFoundException e){
+    public BaseResponse<?> handleNotFoundException(NotFoundException e) {
         String eventId = MDC.get(REQUEST_KEY);
         log.info("eventId = {} ", eventId, e);
         return new BaseResponse<>(e.getStatus());
@@ -74,8 +70,7 @@ public class ExceptionAdvisor {
     // 클라이언트와 서버의 상태 충돌이 일어날 경우 에러 핸들러
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(ConflictException.class)
-    @ResponseBody
-    public BaseResponse<?> handleConflictException(ConflictException e){
+    public BaseResponse<?> handleConflictException(ConflictException e) {
         String eventId = MDC.get(REQUEST_KEY);
         log.info("eventId = {} ", eventId, e);
         return new BaseResponse<>(e.getStatus());
@@ -84,8 +79,7 @@ public class ExceptionAdvisor {
     // 비즈니스 로직 에러 핸들러
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(BaseException.class)
-    @ResponseBody
-    public BaseResponse<?> handleBaseException(BaseException e){
+    public BaseResponse<?> handleBaseException(BaseException e) {
         String eventId = MDC.get(REQUEST_KEY);
         log.info("eventId = {} ", eventId, e);
         return new BaseResponse<>(e.getStatus());
@@ -94,8 +88,7 @@ public class ExceptionAdvisor {
     // 서버 에러 핸들러
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    @ResponseBody
-    public BaseResponse<?> handleException(Exception e){
+    public BaseResponse<?> handleException(Exception e) {
         String eventId = MDC.get(REQUEST_KEY);
         log.error("eventId = {} ", eventId, e);
         return new BaseResponse<>(SERVER_ERROR);
